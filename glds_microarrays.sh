63d0df51ca5cf20d3923a6b19c4258fa440697d3
hfogle <homer.fogle@nasa.gov>
2021-06-01 08:49:35 -0700

    changed paths for staging files

--- glds_microarrays.sh
+++ glds_microarrays.sh
@@ -1,17 +1,21 @@
 #!/bin/bash
 
 
-workdir=pwd
+workdir=$(pwd)
 tmpdir=$(mktemp -d -t ci-XXXXXXXXXXXXXXXXXXXX)
-cd $tmpdir
-echo $tmpdir
+mkdir -p $workdir/Processed_Data
+mkdir -p $workdir/Processed_Data/$1
+outdir=$workdir/Processed_Data/$1
+
+echo $workdir
+echo $outdir
 #eval "$(conda shell.bash hook)"
 #conda activate glds_microarrays
-
+cd $outdir
 retrieve_isa_from_genelab.py --accession $1 --to-Microarray-runsheet --allow-missing-columns 
 
 echo $1
 cd $workdir
-Rscript --vanilla glds_microarrays.R --staging $tmpdir/*.csv --glds $1 --isa $tmpdir/*ISA.zip
+Rscript --vanilla $workdir/glds_microarrays.R --staging $outdir/*.csv --glds $1 --isa $outdir/*ISA.zip
 
 rm -rf $tmpdir
